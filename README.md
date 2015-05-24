b464a6a83d3b73bf6257ba7db23e3fb3f9737813
xjflyttp <xjflyttp@gmail.com>
2015-05-24 10:15:39 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# yii2-tag-it-widget
+# yii2-tagit-widget
+```
 https://github.com/aehlke/tag-it
+```
+
+composer.json
+---------
+```json
+"require": {
+    "xj/yii2-tagit-widget": "*"
+},
+```
+
+In View
+---------
+```php
+use xj\tagit\TagitAsset;
+TagitAsset::register($this);
+```
---
+++ TagitAsset.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * 
+ * @author xjflyttp <xjflyttp@gmail.com>
+ */
+
+namespace xj\tagit;
+
+use yii\web\AssetBundle;
+
+class TagitAsset extends AssetBundle
+{
+
+    public $sourcePath = '@bower/tag-it';
+    public $basePath = '@webroot/assets';
+    public $js = ['js/tag-it.js'];
+    public $css = [
+        'css/jquery.tagit.css',
+        'css/tagit.ui-zendesk.css',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\jui\JuiAsset',
+    ];
+
+}
